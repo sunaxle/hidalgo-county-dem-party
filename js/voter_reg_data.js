2454a475cc855ffacc0290df11d024350baf1d60
Desiderio Romero <desiderio@hidalgocountydems.org>
2026-06-05 15:38:57 -0500

    feat: Add historical voter registration data to Live Tracker

--- js/voter_reg_data.js
+++ js/voter_reg_data.js
@@ -1,3 +1,26 @@
+const historicalVoterData = [
+  // Primary Elections
+  { year: 2006, type: 'Primary Election', count: 271132 },
+  { year: 2008, type: 'Primary Election', count: 291301 },
+  { year: 2012, type: 'Primary Election', count: 293523 },
+  { year: 2014, type: 'Primary Election', count: 309275 },
+  { year: 2016, type: 'Primary Election', count: 317730 },
+  { year: 2018, type: 'Primary Election', count: 349735 },
+  { year: 2020, type: 'Primary Election', count: 379658 },
+  { year: 2022, type: 'Primary Election', count: 405377 },
+  { year: 2024, type: 'Primary Election', count: 431335 },
+  // General Elections
+  { year: 2008, type: 'General Election', count: 284144 },
+  { year: 2010, type: 'General Election', count: 297602 },
+  { year: 2012, type: 'General Election', count: 307186 },
+  { year: 2014, type: 'General Election', count: 321564 },
+  { year: 2016, type: 'General Election', count: 338990 },
+  { year: 2018, type: 'General Election', count: 362952 },
+  { year: 2020, type: 'General Election', count: 392604 },
+  { year: 2022, type: 'General Election', count: 418169 },
+  { year: 2024, type: 'General Election', count: 447359 }
+];
+
 const voterRegistrationData = [
   { date: '2026-05-01', count: 458120 },
   { date: '2026-05-05', count: 458600 },
