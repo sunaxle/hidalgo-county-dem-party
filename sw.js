10031972e168f48312b88c004be7c2769280d53b
Desiderio Romero <romerodeab@gmail.com>
2026-09-17 21:58:56 -0500

    Fix Bluehost deployment to sync to public_html and root, bust SW cache, add no-cache headers

--- sw.js
+++ sw.js
@@ -1,57 +1,20 @@
-const CACHE_NAME = 'join-app-v3';
-const urlsToCache = [
-  '/join.html',
-  '/css/state_party_clone.css',
-  '/css/a11y.css',
-  '/js/app.js',
-  '/js/form_handler.js',
-  '/js/value_counter.js',
-  '/emails/welcome.html',
-  '/images/facebook_1656248751972_6946810765393131439.webp',
-  '/images/hidalgo_dems_logo.png',
-  '/images/icon-192.png',
-  '/images/icon-512.png',
-  '/manifest.json'
-];
+const CACHE_NAME = 'join-app-v4-flush';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).catch(() => {
-          // If network request fails and it's a navigation request, try to return join.html
-          if (event.request.mode === 'navigate') {
-            return caches.match('/join.html');
-          }
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  // Always fetch fresh network response
+  event.respondWith(fetch(event.request));
 });
